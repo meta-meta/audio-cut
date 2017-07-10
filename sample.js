d8aced1aef6e34179e61b3d7ac2791a98d4df51a
Paul Michael Christian <paulmchristian@gmail.com>
2017-07-10 18:20:33 -0500

    upload file and create a transcoder job

--- sample.js
+++ sample.js
@@ -1,36 +1,57 @@
-/*
- * Copyright 2013. Amazon Web Services, Inc. All Rights Reserved.
- * 
- * Licensed under the Apache License, Version 2.0 (the "License");
- * you may not use this file except in compliance with the License.
- * You may obtain a copy of the License at
- * 
- *     http://www.apache.org/licenses/LICENSE-2.0
- * 
- * Unless required by applicable law or agreed to in writing, software
- * distributed under the License is distributed on an "AS IS" BASIS,
- * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
- * See the License for the specific language governing permissions and
- * limitations under the License.
-**/
+const AWS = require('aws-sdk');
+const uuid = require('node-uuid');
+const fs = require('fs');
 
-// Load the SDK and UUID
-var AWS = require('aws-sdk');
-var uuid = require('node-uuid');
+const s3 = new AWS.S3();
+const transcoder = new AWS.ElasticTranscoder({ region: 'us-west-2' });
 
-// Create an S3 client
-var s3 = new AWS.S3();
 
-// Create a bucket and upload something into it
-var bucketName = 'node-sdk-sample-' + uuid.v4();
-var keyName = 'hello_world.txt';
+const audioRawBucket = 'audio-raw';
+const filename = '08 - Looping Steps.mp3';
 
-s3.createBucket({Bucket: bucketName}, function() {
-  var params = {Bucket: bucketName, Key: keyName, Body: 'Hello World!'};
-  s3.putObject(params, function(err, data) {
-    if (err)
-      console.log(err)
-    else
-      console.log("Successfully uploaded data to " + bucketName + "/" + keyName);
+const uploadFile = filename =>
+  fs.readFile(`audio/${filename}`, (err, data) => {
+    if (err) {
+      throw err;
+    }
+
+    s3.upload({
+      Bucket: audioRawBucket,
+      Key: filename,
+      Body: new Buffer(data, 'binary'),
+      ACL: 'public-read'
+    }, (err, data) => {
+      console.log('err:', JSON.stringify(err));
+      console.log('data:', JSON.stringify(data));
+      // console.log(arguments);
+      console.log('Successfully uploaded file.');
+    });
+
   });
+
+// uploadFile(filename);
+
+const audioCutPipelineId = '1499704615497-2bsssf'; // audio-cut
+const flacPresetId = '1499706146017-1vv8v0'; // flac copy
+
+transcoder.createJob({
+  PipelineId: audioCutPipelineId,
+  Input: {
+    Key: filename,
+  },
+  Outputs: [
+    {
+      Key: filename + '-cut1.flac',
+      PresetId: flacPresetId,
+      Composition: [{
+        TimeSpan: {
+          StartTime: '00:00:05.000', //HH:mm:ss.SSS or sssss.SSS
+          Duration: '00:00:10.000',
+        }
+      }]
+    }
+  ]
+}, (err, data) => {
+  console.log('err:', JSON.stringify(err));
+  console.log('data:', JSON.stringify(data));
 });
